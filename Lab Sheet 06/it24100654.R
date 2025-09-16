96deaee465cecd1665dcf1a39f7e75866c053c01
it24100654 <IT24100654@my.sliit.lk>
2025-09-16 16:10:22 +0530

    Add files via upload

---
+++ Lab Sheet 06/it24100654.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\MSI\\Desktop\\it24100654-L6")
+##Q1.i
+# binomial distribution
+
+
+##Q1.ii
+pbinom(46,50,0.85,lower.tail =FALSE)
+
+
+##Q2.i
+#number of customer calls per hour
+
+
+##Q2.ii
+#poisson distribution
+
+#Q2.iii
+dpois(15,12)
+
