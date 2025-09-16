2d4c28bf4f496673e06abe93104af3a75efef05a
it24100654 <IT24100654@my.sliit.lk>
2025-09-16 15:36:24 +0530

    Add files via upload

---
+++ Lab Sheet 07/PS-Lab-7.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\MSI\\Desktop\\it24100654-L7")
+##Q1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+##Q2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+
+##Q3 part 1
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+
+##Q3 part 2
+qnorm(0.95, mean=100, sd=15)
